389ef657c7f7dc502b9a77833a7c42e6938a5550
Jack Curtis <jack.curtis@kcl.ac.uk>
2022-04-10 21:24:57 +0100

    Working mprotect test

--- mprotectviolation.c
+++ mprotectviolation.c
@@ -4,8 +4,9 @@
 int main(int argc, char *argv[])
 {
     printf(1, "Lets try to protect some memory!\n");
-    int protectThis = 16;
-    mprotect(&protectThis, sizeof(protectThis));
+    int *p = (int *)sbrk(1);
+    mprotect(p, 1);
+    *p = 100;
     printf(1, "We shouldn't have gotten here!\n");
     exit();
 }
